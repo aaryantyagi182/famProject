3f0dbb2c9bf7bb666ec1cba325537b041629d294
Aaryan Tyagi <aatyagi293@gmail.com>
2024-04-08 11:27:15 +0530

    Add new API key

--- famProject/Youtube/constants.py
+++ famProject/Youtube/constants.py
@@ -1,4 +1,4 @@
 
 SEARCH_API = 'https://www.googleapis.com/youtube/v3/search'
 API_KEY = 'AIzaSyAsnqqwn_HmljC22p8X3fbptJZdY1JgLfI'
-API_KEY_2 = ''
+API_KEY_2 = 'AIzaSyC8fhtxtPYL_VQ1_GXaiWXtjdxHSh-ID5M'
